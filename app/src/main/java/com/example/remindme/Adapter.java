f030b62af243cfd108c9dd9944c132181700a505
abdullahsultan <abdullahsultan448@gmail.com>
2021-01-11 15:43:37 +0500

    Added Adapter code

--- app/src/main/java/com/example/remindme/Adapter.java
+++ app/src/main/java/com/example/remindme/Adapter.java
@@ -1,33 +1,54 @@
 package com.example.remindme;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
+    Context context;
+    ArrayList<ReminderItems> data;
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.reminder_items,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        ReminderItems reminderItems = this.data.get(position);
+        holder.title.setText(reminderItems.title);
+        String temp = Integer.toString(reminderItems.time_hours)+":" + Integer.toString(reminderItems.time_minutes)+":"+ reminderItems.time_AM_PM;
+        holder.time.setText(temp);
+        temp = Integer.toString(reminderItems.date_day)+"/"+Integer.toString(reminderItems.date_month)+"/"+Integer.toString(reminderItems.date_year);
+        holder.date.setText(temp);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+            return this.data.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView title;
+        TextView time;
+        TextView date;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.reminder_name);
+            time = itemView.findViewById(R.id.reminder_time);
+            date= itemView.findViewById(R.id.reminder_date);
         }
     }
 }
--- app/src/main/java/com/example/remindme/ReminderItems.java
+++ app/src/main/java/com/example/remindme/ReminderItems.java
@@ -1,4 +1,33 @@
 package com.example.remindme;
 
 public class ReminderItems {
+
+    String title;
+
+    int time_hours;
+    int time_minutes;
+    String time_AM_PM;
+
+    int date_day;
+    int date_month;
+    int date_year;
+
+
+    public void set_title(String title)
+    {
+        this.title =title;
+    }
+
+    public void set_time(int time_hours, int time_minutes,String time_AM_PM){
+        this.time_hours = time_hours;
+        this.time_minutes = time_minutes;
+        this.time_AM_PM = time_AM_PM;
+    }
+
+    public void set_date(int date_day,int date_month, int date_year){
+        this.date_day = date_day;
+        this.date_month = date_month;
+        this.date_year = date_year;
+    }
+
 }
